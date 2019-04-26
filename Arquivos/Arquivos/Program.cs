a99da99360ae047822e9520e215c27193d6e241d
Alexandre Dionizio <alexandre.dionizio@sp.senac.br>
2019-04-26 20:55:16 -0300

    Primeiro Commit

---
+++ Arquivos/Arquivos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Arquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(@"C:\VS\teste.txt",FileMode.Open);
+                StreamReader sr = new StreamReader(fs);
+                string linha = sr.ReadLine();
+                Console.WriteLine(linha);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("Arquivo não encontrado",e.Message);
+            }
+            finally
+            {
+                if (fs != null)
+                {
+                    fs.Close();
+                }
+            }
+        }
+    }
+}
