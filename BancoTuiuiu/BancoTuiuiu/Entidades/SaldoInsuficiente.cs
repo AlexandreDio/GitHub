54817cf2d7e1e3bebe1884ce02967f76043404df
Alexandre Dionizio <alexandre.dionizio@sp.senac.br>
2019-04-26 22:24:24 -0300

    Adicionado a classe SaldoInsuficiente e os arquivos da aula

---
+++ BancoTuiuiu/BancoTuiuiu/Entidades/SaldoInsuficiente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoTuiuiu.Entidades
+{
+    class SaldoInsuficiente : Exception
+    {
+        public SaldoInsuficiente(string mensagem) : base(mensagem)
+        {
+
+        }
+    }
+}
